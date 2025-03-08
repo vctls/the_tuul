f764edc180390c128a6327e07df6d0d07a10a619
Dan Kurtz <andfurthermore@gmail.com>
2025-03-07 21:34:51 -0800

    Switch from jsdom to happy-dom
    Better API support, apparently

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     test: {
         globals: true,
-        environment: 'jsdom',
+        environment: 'happy-dom',
         setupFiles: ['./vitest.setup.ts'],
         include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
         exclude: ['**/node_modules/**', '**/dist/**'],
