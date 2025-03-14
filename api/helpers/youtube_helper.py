08bca1d63580cf0082e51d0655cc76f1b40ec43a
Dan Kurtz <andfurthermore@gmail.com>
2025-03-13 19:43:00 -0700

    Accept a YOUTUBE_SOCKS5_PROXY url

--- api/helpers/youtube_helper.py
+++ api/helpers/youtube_helper.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import structlog
 
 import pytubefix as pytube
+from django.conf import settings
 
 logger = structlog.get_logger(__name__)
 
@@ -15,7 +16,14 @@ def get_youtube_streams(
     Return audio and video paths.
     """
 
-    youtube = pytube.YouTube(youtube_url)
+    proxy_options = None
+    if settings.YOUTUBE_SOCKS5_PROXY:
+        proxy_options = {
+            "socks5": settings.YOUTUBE_SOCKS5_PROXY,
+        }
+        logger.info("Using proxy for YouTube download")
+
+    youtube = pytube.YouTube(youtube_url, proxies=proxy_options)
     audio_stream = youtube.streams.filter(only_audio=True).first()
     video_stream = youtube.streams.filter(only_video=True, res="1080p").first()
     if not video_stream:
