23de24341550d534e072bfa3daa1ee61902fb8a4
Dan Kurtz <andfurthermore@gmail.com>
2025-04-09 22:00:08 -0700

    refactor: specify fontawesome icons to reduce bundle size
    See plugins/fontawesome.ts to add new icons.

---
+++ frontend/plugins/fontawesome.ts
@@ -0,0 +1,59 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Vue from 'vue';
+
+// Import only the specific icons we need
+import {
+  faDownload,
+  faCopy,
+  faUpload,
+  faTrashCan,
+  faFileAudio,
+  // faLoader, // This icon doesn't exist in FontAwesome
+  faCircleQuestion,
+  faAngleDown,
+  faAngleRight,
+  faCircleDollarToSlot,
+  faBlender,
+  faCircleExclamation,
+  faCircleInfo,
+  faAlignLeft,
+  faFlask,
+  faStopwatch,
+  faQuestion
+} from '@fortawesome/free-solid-svg-icons';
+
+// Import from brands
+import {
+  faGithub
+} from '@fortawesome/free-brands-svg-icons';
+
+// Add only the imported icons to the library
+library.add(
+  // Solid icons
+  faDownload,
+  faCopy,
+  faUpload,
+  faTrashCan,
+  faFileAudio,
+  // faLoader,
+  faCircleQuestion,
+  faAngleDown,
+  faAngleRight,
+  faCircleDollarToSlot,
+  faBlender,
+  faCircleExclamation,
+  faCircleInfo,
+  faAlignLeft,
+  faFlask,
+  faStopwatch,
+  faQuestion,
+  
+  // Brand icons
+  faGithub
+);
+
+// Register the component globally
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+export default FontAwesomeIcon;
